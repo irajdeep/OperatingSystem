daf6ae773f2c94a9eeb469ffaa9de61984aa0ca6
Rajdeep Das <rajdeep.das@booking.com>
2017-12-25 11:30:01 +0100

    Added compiling instruction to link pthread

--- PThreadSampleCode /pthread-creation-quiz-1.c
+++ PThreadSampleCode /pthread-creation-quiz-1.c
@@ -1,7 +1,7 @@
 /* PThread Creation Quiz 1 */ 
 
 #include <stdio.h>
-#include <pthread.h>
+#include <pthread.h> //Include pthread library
 
 #define NUM_THREADS 4
 
@@ -10,12 +10,19 @@ void *hello (void *arg) { /* thread main */
 	return 0;
 }
 
+/*int pthread_create(pthread_t *thread , const pthread_attr_t *attr, 
+	void * (*start_routine)(void *), void *arg);*/
+
+/*
+comile the code => gcc -o pthread-creation-quiz-1 pthread-creation-quiz-1.c -lpthread
+*/
 int main (void) {
 	int i;
 	pthread_t tid[NUM_THREADS];
 	
 	for (i = 0; i < NUM_THREADS; i++) { /* create/fork threads */
-		pthread_create(&tid[i], NULL, hello, NULL);
+		pthread_create(&tid[i], NULL, hello, NULL); 
+
 	}
 	
 	for (i = 0; i < NUM_THREADS; i++) { /* wait/join threads */
